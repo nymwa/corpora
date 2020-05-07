743178e37a74e22ce3fd0249852cbf6460651300
nymwa <nymwa0@gmail.com>
2020-05-07 23:45:27 +0900

    add

---
+++ conll/download.sh
@@ -0,0 +1,6 @@
+wget -qnc https://www.comp.nus.edu.sg/~nlp/conll14st/conll14st-test-data.tar.gz
+wget -qnc https://www.comp.nus.edu.sg/~nlp/conll14st/official_submissions.tar.gz
+wget -qnc https://www.comp.nus.edu.sg/~nlp/conll13st/release2.3.1.tar.gz
+tar zxvf conll14st-test-data.tar.gz
+tar zxvf official_submissions.tar.gz
+tar zxvf release2.3.1.tar.gz
